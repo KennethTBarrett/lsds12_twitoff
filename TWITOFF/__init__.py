d21a6fd182eb30f886514e8f2bb79acaf7bb584c
Kenneth T Barrett <KennethTBarrett@criptext.com>
2020-03-24 04:26:32 -0500

    Module 2 update

--- TWITOFF/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Entry point for Twitoff Flask app"""
-
-from .app import create_app
-
-# APP is a global variable
-APP = create_app()
-
-# Run this in terminal with FLASK_APP=TWITOFF:APP flask run
--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,19 +1,22 @@
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
+
 
 # Now, we want to make an app factory.
-
 
 def create_app():
     app = Flask(__name__)
 
     # Add our config
-    app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///db.sqlite3')
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Have the database know about the app.
     DB.init_app(app)
 
     @app.route('/')
     def route():
-        return 'Welcome to Twitoff!'
+        users = User.query.all()
+        return render_template('index.html', title='Home', users=users)
     return app
